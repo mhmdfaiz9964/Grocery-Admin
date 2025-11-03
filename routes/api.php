2593c6678d8b355f511d3893fe5f222c03294a7d
Mohammed Faiz <145247205+mhmdfaiz9964@users.noreply.github.com>
2025-11-03 17:21:46 +0530

    Updated Issues 0002

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Passport;
-use App\Http\Controllers\Api\ShopController;
+use App\Http\Controllers\API\ShopController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
